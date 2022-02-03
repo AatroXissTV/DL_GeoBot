ecdb2ecf619f8a1e6cfa04221cc988c6a6887495
AatroXiss <antoine.beaudesson@gmail.com>
2022-02-03 12:23:12 +0100

    v.0.0.1 first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# main.py
+# created 03/02/2022 at 12:02 by Antoine 'AatroXiss' BEAUDESSON
+# last modified 03/02/2022 at 12:02 by Antoine 'AatroXiss' BEAUDESSON
+
+""" main.py:
+    - *
+"""
+
+__author__ = "Antoine 'AatroXiss' BEAUDESSON"
+__copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
+__credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
+__license__ = ""
+__version__ = "0.0.1"
+__maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
+__email__ = "antoine.beaudesson@gmail.com"
+__status__ = "Development"
+
+# standard library imports
+
+# third party imports
+
+# local application imports
+
+# other imports
+
+# constants
