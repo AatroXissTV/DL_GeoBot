e8fbe28282dd6282adb230f8a76a3db7318fcbe6
AatroXiss <antoine.beaudesson@gmail.com>
2022-02-04 11:00:46 +0100

    v.0.0.3 data preprocessing and augmentation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = ""
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import seaborn as sns
+from keras.preprocessing.image import ImageDataGenerator
 
 # local application imports
 
@@ -54,6 +55,7 @@ def get_data(dataset_dir):
 
 
 train = get_data("dataset/train")
+test = get_data("dataset/test")
 
 train_list = []
 for i in train:
@@ -64,3 +66,45 @@ for i in train:
 sns.set_style("darkgrid")
 sns.countplot(train_list)
 plt.show()
+
+# Data Preprocessing
+
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+
+for feature, label in train:
+    x_train.append(feature)
+    y_train.append(label)
+
+for feature, label in test:
+    x_test.append(feature)
+    y_test.append(label)
+
+
+# Normalize the data
+x_train = np.array(x_train) / 255
+x_test = np.array(x_test) / 255
+
+x_train.reshape(-1, img_size, img_size, 1)
+y_train = np.array(y_train)
+
+x_test.reshape(-1, img_size, img_size, 1)
+y_test = np.array(y_train)
+
+
+# Data Augmentation
+datagen = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=30,
+    zoom_range=0.2,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=False,
+    vertical_flip=False
+)
